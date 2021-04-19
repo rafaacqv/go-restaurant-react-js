8b263dfdfbc8364b222613562be3eaccea0d593a
Rafael Araujo <rafaacq@gmail.com>
2021-04-19 18:00:02 -0300

    Refactor Food component

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface StyledComponentProps {
+  available: boolean,
+}
+
+export const Container = styled.div<StyledComponentProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
